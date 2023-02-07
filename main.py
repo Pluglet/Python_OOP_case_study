ca3401160987a01a4d81243d67ce47a9f5603d98
Pluglet <Pluglet@users.noreply.github.com>
2023-02-07 16:40:12 +0800

    Fouth commit - changes in main.py - chapter 3 completed.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+from pathlib import Path
+class AudioFile:
+    ext: str
+    def __init__(self, filepath: Path) -> None:
+        if not filepath.suffix == self.ext:
+            raise ValueError("Invalid file format")
+        self.filepath = filepath
+class MP3File(AudioFile):
+    ext = ".mp3"
+    def play(self) -> None:
+        print(f"playing {self.filepath} as mp3")
+class WavFile(AudioFile):
+    ext = ".wav"
+    def play(self) -> None:
+        print(f"playing {self.filepath} as wav")
+class OggFile(AudioFile):
+    ext = ".ogg"
+    def play(self) -> None:
+        print(f"playing {self.filepath} as ogg")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+p_1 = MP3File(Path("Heart of the Sunrise.mp3"))
+p_1.play()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
